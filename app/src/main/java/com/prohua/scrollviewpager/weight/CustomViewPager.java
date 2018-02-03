fc9357c48b5e89cfc773892da66a883015b1cf65
deepblue_zou <1023509177@qq.com>
2018-02-03 11:49:46 +0800

    修复bug

--- app/src/main/java/com/prohua/scrollviewpager/weight/CustomViewPager.java
+++ app/src/main/java/com/prohua/scrollviewpager/weight/CustomViewPager.java
@@ -5,6 +5,8 @@ import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ScrollView;
 
 /**
  * 自适应ViewPager
@@ -16,8 +18,9 @@ public class CustomViewPager extends ViewPager {
     private static final String TAG = "CustomViewPager";
 
     private int position = 0;
+
     public CustomViewPager(Context context) {
-        this(context,null);
+        this(context, null);
     }
 
     public CustomViewPager(Context context, AttributeSet attrs) {
@@ -42,21 +45,21 @@ public class CustomViewPager extends ViewPager {
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
 
-        if(position!=0) {
-            int height = 0;
-            for (int i = 0; i < getChildCount(); i++) {
-                View child = getChildAt(i);
-                child.measure(widthMeasureSpec,
-                        MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
-                int h = child.getMeasuredHeight();
-                if (h > height)
-                    height = h;
-                Log.i("height", "" + height);
+        boolean have = false;
+
+        if (getChildAt(position) instanceof ViewGroup) {
+            ViewGroup childView = (ViewGroup) getChildAt(position);
+            for (int j = 0; j < childView.getChildCount(); j++) {
+                View childChildView = childView.getChildAt(j);
+                if (childChildView instanceof ScrollView) {
+                    have = true;
+                    Log.i(TAG, "ViewGroup have ScrollView");
+                }
             }
-            heightMeasureSpec = MeasureSpec.makeMeasureSpec(height,
-                    MeasureSpec.EXACTLY);
-            super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        } else {
+        }
+
+        // ScrollView
+        if (have) {
             super.onMeasure(widthMeasureSpec, heightMeasureSpec);
             Log.d(TAG, "onMeasure");
             // find the current child view
@@ -67,6 +70,20 @@ public class CustomViewPager extends ViewPager {
             }
 
             setMeasuredDimension(getMeasuredWidth(), measureHeight(heightMeasureSpec, view));
+        } else {
+            int height = 0;
+            for (int i = 0; i < getChildCount(); i++) {
+                View child = getChildAt(i);
+                child.measure(widthMeasureSpec,
+                        MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
+                int h = child.getMeasuredHeight();
+                if (h > height)
+                    height = h;
+                Log.i(TAG, "MeasuredHeight:" + height);
+            }
+            heightMeasureSpec = MeasureSpec.makeMeasureSpec(height,
+                    MeasureSpec.EXACTLY);
+            super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         }
     }
 
@@ -74,8 +91,7 @@ public class CustomViewPager extends ViewPager {
      * Determines the height of this view
      *
      * @param measureSpec A measureSpec packed into an int
-     * @param view the base view with already measured height
-     *
+     * @param view        the base view with already measured height
      * @return The height of the view, honoring constraints from measureSpec
      */
     private int measureHeight(int measureSpec, View view) {
