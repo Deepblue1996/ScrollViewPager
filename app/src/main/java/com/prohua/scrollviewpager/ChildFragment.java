fa0daa932b8567a2774dab2263efa0c2b5010e0e
deepblue_zou <1023509177@qq.com>
2018-02-03 11:29:37 +0800

    添加ScrollView->ViewPager->ScrollView自适应

--- app/src/main/java/com/prohua/scrollviewpager/ChildFragment.java
+++ app/src/main/java/com/prohua/scrollviewpager/ChildFragment.java
@@ -8,7 +8,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.prohua.scrollviewpager.weight.CustomViewPager;
 import com.prohua.universal.DefaultAdapter;
 import com.prohua.universal.DefaultViewHolder;
 
--- app/src/main/java/com/prohua/scrollviewpager/weight/CustomViewPager.java
+++ app/src/main/java/com/prohua/scrollviewpager/weight/CustomViewPager.java
@@ -13,28 +13,88 @@ import android.view.View;
 
 public class CustomViewPager extends ViewPager {
 
+    private static final String TAG = "CustomViewPager";
+
+    private int position = 0;
     public CustomViewPager(Context context) {
-        super(context);
+        this(context,null);
     }
 
     public CustomViewPager(Context context, AttributeSet attrs) {
         super(context, attrs);
+        addOnPageChangeListener(new OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                CustomViewPager.this.position = position;
+                requestLayout();
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+            }
+        });
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        int height = 0;
-        for (int i = 0; i < getChildCount(); i++) {
-            View child = getChildAt(i);
-            child.measure(widthMeasureSpec,
-                    MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
-            int h = child.getMeasuredHeight();
-            if (h > height)
-                height = h;
-            Log.i("height",""+height);
+
+        if(position!=0) {
+            int height = 0;
+            for (int i = 0; i < getChildCount(); i++) {
+                View child = getChildAt(i);
+                child.measure(widthMeasureSpec,
+                        MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
+                int h = child.getMeasuredHeight();
+                if (h > height)
+                    height = h;
+                Log.i("height", "" + height);
+            }
+            heightMeasureSpec = MeasureSpec.makeMeasureSpec(height,
+                    MeasureSpec.EXACTLY);
+            super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        } else {
+            super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+            Log.d(TAG, "onMeasure");
+            // find the current child view
+            View view = getChildAt(getCurrentItem());
+            if (view != null) {
+                // measure the current child view with the specified measure spec
+                view.measure(widthMeasureSpec, heightMeasureSpec);
+            }
+
+            setMeasuredDimension(getMeasuredWidth(), measureHeight(heightMeasureSpec, view));
         }
-        heightMeasureSpec = MeasureSpec.makeMeasureSpec(height,
-                MeasureSpec.EXACTLY);
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
     }
+
+    /**
+     * Determines the height of this view
+     *
+     * @param measureSpec A measureSpec packed into an int
+     * @param view the base view with already measured height
+     *
+     * @return The height of the view, honoring constraints from measureSpec
+     */
+    private int measureHeight(int measureSpec, View view) {
+        int result = 0;
+        int specMode = MeasureSpec.getMode(measureSpec);
+        int specSize = MeasureSpec.getSize(measureSpec);
+
+        if (specMode == MeasureSpec.EXACTLY) {
+            result = specSize;
+        } else {
+            // set the height from the base view if available
+            if (view != null) {
+                result = view.getMeasuredHeight();
+            }
+            if (specMode == MeasureSpec.AT_MOST) {
+                result = Math.min(result, specSize);
+            }
+        }
+        return result;
+    }
+
 }
